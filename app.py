ee2164d2bb99615efcf0de4c06b43f6fd724b04f
s-kp <ksushwet@gmail.com>
2022-06-02 15:09:18 +0530

    updated app.py and requirements.txt

---
+++ app.py
@@ -0,0 +1,65 @@
+
+import streamlit as st
+from keras.models import load_model
+# from streamlit import session_state
+# import av
+import cv2
+import numpy as np
+from time import sleep
+from tensorflow.keras.preprocessing.image import img_to_array
+
+
+
+
+classifier = load_model('mobilenet.h5')
+
+class_labels = {0: 'angry', 1: 'disgust', 2: 'fear', 3: 'happy', 4: 'neutral', 5: 'sad', 6: 'surprise'}
+
+face_classifier = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+st.title("Face Emotion Detection")
+
+FRAME_WINDOW=st.image([])
+
+
+def face_detector(img):
+    # Convert image to grayscale
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    faces = face_classifier.detectMultiScale(gray, 1.3, 5)
+    if faces is ():
+        return (0,0,0,0), np.zeros((48,48), np.uint8), img
+    for (x,y,w,h) in faces:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+        roi_gray = gray[y:y+h, x:x+w]
+
+    try:
+        roi_gray = cv2.resize(roi_gray, (48, 48), interpolation = cv2.INTER_AREA)
+    except:
+        return (x,w,y,h), np.zeros((48,48), np.uint8), img
+    return (x,w,y,h), roi_gray, img
+
+cap = cv2.VideoCapture(0)
+
+
+while True:
+
+    ret, frame = cap.read()
+    rect, face, image = face_detector(frame)
+    if np.sum([face]) != 0.0:
+        roi = face.astype("float") / 255.0
+        roi = img_to_array(roi)
+        roi = np.expand_dims(roi, axis=0)
+
+        # make a prediction on the ROI, then lookup the class
+        preds = classifier.predict(roi)[0]
+        label = class_labels[preds.argmax()]  
+        label_position = (rect[0] + int((rect[1]/2)), rect[2] + 25)
+        cv2.putText(image, label, label_position , cv2.FONT_HERSHEY_SIMPLEX,2, (0,255,0), 3)
+        image=cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
+    else:
+        cv2.putText(image, "No Face Found", (20, 60) , cv2.FONT_HERSHEY_SIMPLEX,2, (0,255,0), 3)
+        
+    # cv2.imshow('All', image)
+    FRAME_WINDOW.image(image)
+
+
